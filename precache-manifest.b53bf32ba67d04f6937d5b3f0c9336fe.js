5faf2178a0f13a95d7dad7c727e57d4b354a19f0
AFYCHRIST <nanaafia178@gmail.com>
2020-09-10 19:57:41 +0100

    Updates

---
+++ precache-manifest.b53bf32ba67d04f6937d5b3f0c9336fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "991cc696420d5d021ad95a2b5cd51420",
+    "url": "/HW6_AYC6LJSTH7/index.html"
+  },
+  {
+    "revision": "585af53529c3659fb842",
+    "url": "/HW6_AYC6LJSTH7/static/css/main.b9a29853.chunk.css"
+  },
+  {
+    "revision": "d2e373c254585c9be00e",
+    "url": "/HW6_AYC6LJSTH7/static/js/2.a7f2996d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HW6_AYC6LJSTH7/static/js/2.a7f2996d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "585af53529c3659fb842",
+    "url": "/HW6_AYC6LJSTH7/static/js/main.3e149044.chunk.js"
+  },
+  {
+    "revision": "f0385eaa95f3a618b269",
+    "url": "/HW6_AYC6LJSTH7/static/js/runtime-main.ae9fecf8.js"
+  },
+  {
+    "revision": "20700fca0591801277e83847d0a4d1cb",
+    "url": "/HW6_AYC6LJSTH7/static/media/logo.20700fca.PNG"
+  }
+]);
